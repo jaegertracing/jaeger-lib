2d245f5e35366a14e78215ce7d2cc52615af2061
Won Jun Jang <wjang@uber.com>
2017-02-07 16:09:23 -0500

    Address comments

--- metrics/testutils/testutils.go
+++ metrics/testutils/testutils.go
@@ -17,30 +17,16 @@ type ExpectedMetric struct {
 
 // TODO do something similar for Timers
 
-// AssertCounterMetric checks if counter metric exists.
-func AssertCounterMetric(t *testing.T, f *metrics.LocalFactory, expectedMetrics ...ExpectedMetric) {
+// AssertCounterMetrics checks if counter metrics exist.
+func AssertCounterMetrics(t *testing.T, f *metrics.LocalFactory, expectedMetrics ...ExpectedMetric) {
 	counters, _ := f.Snapshot()
 	assertMetrics(t, counters, expectedMetrics...)
 }
 
-// AssertGaugeMetric checks if gauge metric exists.
-func AssertGaugeMetric(t *testing.T, f *metrics.LocalFactory, expectedMetrics ...ExpectedMetric) {
+// AssertGaugeMetrics checks if gauge metrics exist.
+func AssertGaugeMetrics(t *testing.T, f *metrics.LocalFactory, expectedMetrics ...ExpectedMetric) {
 	_, gauges := f.Snapshot()
 	assertMetrics(t, gauges, expectedMetrics...)
-}
-
-// AssertCounterMetrics checks if the existing counter metrics exactly match the expected metrics.
-func AssertCounterMetrics(t *testing.T, f *metrics.LocalFactory, expectedMetrics []ExpectedMetric) {
-	counters, _ := f.Snapshot()
-	assertMetrics(t, counters, expectedMetrics...)
-	assert.Len(t, expectedMetrics, len(counters))
-}
-
-// AssertGaugeMetrics checks if the existing gauge metrics exactly match the expected metrics.
-func AssertGaugeMetrics(t *testing.T, f *metrics.LocalFactory, expectedMetrics []ExpectedMetric) {
-	_, gauges := f.Snapshot()
-	assertMetrics(t, gauges, expectedMetrics...)
-	assert.Len(t, expectedMetrics, len(gauges))
 }
 
 func assertMetrics(t *testing.T, actualMetrics map[string]int64, expectedMetrics ...ExpectedMetric) {
