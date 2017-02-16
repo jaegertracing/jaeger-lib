00987e245213cf43cae352a0adc7e3b66943d7ff
Won Jun Jang <wjang@uber.com>
2017-02-16 14:57:18 -0500

    Add zap logger

---
+++ log/logger_test.go
@@ -0,0 +1,12 @@
+package log
+
+import (
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+	for _, logger := range []Logger{StdLogger, NullLogger} {
+		logger.Infof("Hi %s", "there")
+		logger.Error("Bad wolf")
+	}
+}
