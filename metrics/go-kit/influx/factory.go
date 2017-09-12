4fa2796c519a4a603b5db9993cda02a54964f804
Marcin Biegan <marcin.biegan@getbase.com>
2017-09-12 23:51:31 +0200

    Support Influx metrics via go-kit

---
+++ metrics/go-kit/influx/factory.go
@@ -0,0 +1,35 @@
+package influx
+
+import (
+	"github.com/go-kit/kit/metrics"
+	"github.com/go-kit/kit/metrics/influx"
+	"github.com/uber/jaeger-lib/metrics/go-kit"
+)
+
+// NewFactory creates a new metrics factory using go-kit influx package.
+func NewFactory(client *influx.Influx) xkit.Factory {
+	return factory{
+		client: client,
+	}
+}
+
+type factory struct {
+	buckets int
+	client *influx.Influx
+}
+
+func (f factory) Counter(name string) metrics.Counter {
+	return f.client.NewCounter(name)
+}
+
+func (f factory) Histogram(name string) metrics.Histogram {
+	return f.client.NewHistogram(name)
+}
+
+func (f factory) Gauge(name string) metrics.Gauge {
+	return f.client.NewGauge(name)
+}
+
+func (f factory) Capabilities() xkit.Capabilities {
+	return xkit.Capabilities{Tagging: true}
+}
