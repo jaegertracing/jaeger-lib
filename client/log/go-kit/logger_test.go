1a8565c99312c7b06089681fbbaa3e410b40865c
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-09-20 01:53:27 +0200

    Add a Jaeger client logger wrapper around go-kit logger

---
+++ client/log/go-kit/logger_test.go
@@ -0,0 +1,27 @@
+package xkit
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/go-kit/kit/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogger_Infof(t *testing.T) {
+	buf := &bytes.Buffer{}
+	logger := NewLogger(log.NewLogfmtLogger(buf))
+
+	logger.Infof("Formatted %s", "string")
+
+	assert.Equal(t, "level=info msg=\"Formatted string\"\n", buf.String())
+}
+
+func TestLogger_Error(t *testing.T) {
+	buf := &bytes.Buffer{}
+	logger := NewLogger(log.NewLogfmtLogger(buf))
+
+	logger.Error("Something really bad happened")
+
+	assert.Equal(t, "level=error msg=\"Something really bad happened\"\n", buf.String())
+}
