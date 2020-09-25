9f235deb58be92ee6564a0c8c12943f2d24192a7
Daniil Rutskiy <dstdfx@gmail.com>
2020-09-25 11:27:41 +0300

    Tests clean up
    Signed-off-by: Daniil Rutskiy <dstdfx@gmail.com>

--- metrics/fork/fork_test.go
+++ metrics/fork/fork_test.go
@@ -39,94 +39,68 @@ func TestForkFactory(t *testing.T) {
 	ff := New(forkNamespace, forkFactory, defaultFactory)
 	ff.Gauge(metrics.Options{
 		Name: "somegauge",
-		Tags: nil,
-		Help: "",
-	}).Update(666)
+	}).Update(42)
 	ff.Counter(metrics.Options{
 		Name: "somecounter",
-		Tags: nil,
-		Help: "",
 	}).Inc(2)
 
 	// Check that metrics are presented in defaultFactory backend
 	defaultFactory.AssertCounterMetrics(t, metricstest.ExpectedMetric{
 		Name:  "somecounter",
-		Tags:  nil,
 		Value: 2,
 	})
 	defaultFactory.AssertGaugeMetrics(t, metricstest.ExpectedMetric{
 		Name:  "somegauge",
-		Tags:  nil,
-		Value: 666,
+		Value: 42,
 	})
 
 	// Get default namespaced factory
 	defaultNamespacedFactory := ff.Namespace(metrics.NSOptions{
 		Name: "default",
-		Tags: nil,
 	})
 
 	// Add some metrics
 	defaultNamespacedFactory.Counter(metrics.Options{
 		Name: "somenamespacedcounter",
-		Tags: nil,
-		Help: "",
 	}).Inc(111)
 	defaultNamespacedFactory.Gauge(metrics.Options{
 		Name: "somenamespacedgauge",
-		Tags: nil,
-		Help: "",
 	}).Update(222)
 	defaultNamespacedFactory.Histogram(metrics.HistogramOptions{
-		Name:    "somenamespacedhist",
-		Tags:    nil,
-		Help:    "",
-		Buckets: nil,
+		Name: "somenamespacedhist",
 	}).Record(1)
 	defaultNamespacedFactory.Timer(metrics.TimerOptions{
-		Name:    "somenamespacedtimer",
-		Tags:    nil,
-		Help:    "",
-		Buckets: nil,
+		Name: "somenamespacedtimer",
 	}).Record(time.Millisecond)
 
 	// Check values in default namespaced factory backend
 	defaultFactory.AssertCounterMetrics(t, metricstest.ExpectedMetric{
 		Name:  "default.somenamespacedcounter",
-		Tags:  nil,
 		Value: 111,
 	})
 	defaultFactory.AssertGaugeMetrics(t, metricstest.ExpectedMetric{
 		Name:  "default.somenamespacedgauge",
-		Tags:  nil,
 		Value: 222,
 	})
 
 	// Get factory with forkNamespace and add some metrics
 	internalFactory := ff.Namespace(metrics.NSOptions{
 		Name: forkNamespace,
-		Tags: nil,
 	})
 	internalFactory.Gauge(metrics.Options{
 		Name: "someinternalgauge",
-		Tags: nil,
-		Help: "",
 	}).Update(20)
 	internalFactory.Counter(metrics.Options{
 		Name: "someinternalcounter",
-		Tags: nil,
-		Help: "",
 	}).Inc(50)
 
 	// Check that metrics are presented in forkFactory backend
 	forkFactory.AssertGaugeMetrics(t, metricstest.ExpectedMetric{
 		Name:  "internal.someinternalgauge",
-		Tags:  nil,
 		Value: 20,
 	})
 	forkFactory.AssertCounterMetrics(t, metricstest.ExpectedMetric{
 		Name:  "internal.someinternalcounter",
-		Tags:  nil,
 		Value: 50,
 	})
 }
